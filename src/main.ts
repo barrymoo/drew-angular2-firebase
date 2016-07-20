88b682496d6f1188df114d19bd42fc6dd79620c6
Barry E. Moore II <bmooreii@sam-moore.sam.pitt.edu>
2016-07-20 17:14:13 -0400

    Initial Commit, Documentation to Follow

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
-
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase({
+    apiKey: "AIzaSyDn4gNoi-ebXDnMNiW-bZnb9-RTBYB-Oo8",
+    databaseURL: "https://drew-e203c.firebaseio.com",
+    authDomain: "drew-e203c.firebaseapp.com",
+    storageBucket: "drew-e203c.appspot.com"
+  })
+]);
